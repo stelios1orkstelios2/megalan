1530ce3838a2a4c316739ec28649e49274369dc5
stelios1orkstelios2 <stelios1orkstelios2@gmail.com>
2022-09-26 02:28:10 +0300

    epitelous douleuei to ScreenManager mou pire wres na to kanw gia kapoio logo

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivymd.uix.screenmanager import MDScreenManager
+from kivymd.uix.screen import MDScreen
 
-class UserPage(Screen):
+class UserScreen(Screen):
     pass # Den exw idea pws ston poutso na to ftiaksw auto akoma 
 """
     TODO LIST
@@ -12,20 +14,37 @@ class UserPage(Screen):
 
 """
 
+class LoginScreen(Screen):
+    pass
+
+Builder.load_file('user.kv')
 class MainApp(MDApp):
     def build(self):
-
-        sm = ScreenManager()
-        sm.add_widget(UserPage(name='User'))
         self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'BlueGray'
-        return Builder.load_file('my.kv')
+        self.sm = ScreenManager()
+        self.sm.add_widget(UserScreen(name='user_screen'))
+        self.sm.add_widget(LoginScreen(name='loginpage'))
+        self.sm.current = 'loginpage'
+
+        return self.sm
+    
+    def change_screen(self):
+        self.sm.current = 'user_screen'
+        self.sm.transition.direction = 'left'
+        print('button works!')
         
+"""
+    OTAN ME TO KALO FTASOUME STO SIMEIO NA ASXOLITHOUME ME TIN ACTUAL DIADIKASIA 
+
     def logger(self):
-        login = self.root.ids.login.text
+       login = self.root.ids.login.text
+"""
         
 
     def on_start(self):
         self.fps_monitor_start()
 
-MainApp().run()
+if __name__ == "__main__":
+
+    MainApp().run()
