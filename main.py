a6ab7f496b3ae4870d7de28168c8433ab15cd9e9
stelios1orkstelios2 <stelios1orkstelios2@gmail.com>
2022-09-28 17:26:18 +0300

    malakies

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
@@ -5,7 +6,9 @@ from kivy.metrics import dp
 from kivy.core.window import Window
 from kivy.clock import Clock
 from database import MyDB
-
+from kivymd.uix.dialog import MDDialog
+from kivymd.uix.button import MDRectangleFlatButton
+from kivymd.uix.menu import MDDropdownMenu
 """
     Gia to kalo tou sanity mou kalo tha itan na dimiourgisw allo python arxeio to opoio tha diaxeirizete to mysql,
     giati an to kanw auto edw tha mou gamithei i logiki poli asxima :D
@@ -24,66 +27,61 @@ mydb = MyDB()
         2) ftiakse to background color tou splash art na einai idio me to gif na min einai asximo ;D
         2) kali tixi
 
+        MDTopAppBar:
+            id: username_topappbar
+            title: ""
+            left_action_items: [["menu", lambda x: nav_drawer.set_state("open"), "Menu"]]
+            elevation: 10
+            specific_text_color: 1,1,1,1
+            md_bg_color: 0.10980392156862745,0.10196078431372549,0.10980392156862745, 1
+
 """
 
 class MainApp(MDApp):
     def build(self):
-        
         self.theme_cls.theme_style = 'Dark'
         self.theme_cls.primary_palette = 'BlueGray'
-        self.theme_cls.primary_color
+        
         self.sm = ScreenManager()
         self.sm.add_widget(Builder.load_file('splashart.kv'))
         self.sm.add_widget(Builder.load_file('user.kv'))
         self.sm.add_widget(Builder.load_file('login.kv'))
-        """
-        FOR MENU IN TOPAPPBAR OF USERSCREEN
-        menu_items = [
-                    {
-                        "viewclass": "OneLineListItem",
-                        "text": "Log Out",
-                        "height": dp(56),
-                        "on_release": lambda x="": self.menu_callback(x),
-                    } 
-                ]
-        self.menu = MDDropdownMenu(items=menu_items, position="bottom", width_mult=2, ver_growth='down', hor_growth="right")
-        """
+        
         
         
         return self.sm
     
+    def logger(self):
+        password = self.sm.get_screen('loginpage').ids.login.text
+        mydb.cur.execute(f"SELECT * FROM customers WHERE password='{password}'")
+        res = mydb.cur.fetchall()
+        print(res)
+        if len(res) == 0:
+            #This should make a popup where it informs the user that the password was wrong
+            button = MDRectangleFlatButton(text="CLOSE", on_release=lambda x: self.close_dialog(x))
+            self.dialog = MDDialog(title="ERROR!", type="alert", text="Password is incorrect or the user does not exist!", buttons=[button], size_hint=(0.7, None))
+            self.dialog.open()
+            
+        else:
+            self.change_screen('user_screen', 'left')
+            self.sm.get_screen('user_screen').ids.username_topappbar.title = f"Hi, {res[0][0]}" # This is used to grab the input provided by the user and set it as a title in the MDTopAppBar of the UserScreen. This should be used as a way to access the name of a customer and set as greeting in the title of their personal user page
+
+    def close_dialog(self, obj):
+        self.dialog.dismiss()            
+    
     def change_screen(self, screen_choice, direction_choice):
+        
         self.sm.current = screen_choice
         self.sm.transition.direction = direction_choice
-        if self.sm.get_screen('loginpage').ids.login.text.upper() == "":
-            self.sm.get_screen('user_screen').ids.username_topappbar.title = "No user!"
-        else:
-            self.sm.get_screen('user_screen').ids.username_topappbar.title = f"Hi, {self.sm.get_screen('loginpage').ids.login.text.upper()}" # This is used to grab the input provided by the user and set it as a title in the MDTopAppBar of the UserScreen. This should be used as a way to access the name of a customer and set as greeting in the title of their personal user page
         print('button works!') # Used for debuggin purposes 
-    
-    def logout_button(self, button):
-        self.change_screen('loginpage', 'right')
-        
-    
-    def menu_callback(self, text_item):
-        self.change_screen('loginpage', 'right')
-        self.menu.dismiss()
     
     def splash_screen_start(self, *args):
         self.sm.current = 'loginpage'
 
     def on_start(self):
         
-        Clock.schedule_once(self.splash_screen_start, 7)
+        Clock.schedule_once(self.splash_screen_start, 2)
         #self.fps_monitor_start()        
-"""
-    OTAN ME TO KALO FTASOUME STO SIMEIO NA ASXOLITHOUME ME TIN ACTUAL DIADIKASIA 
-
-    def logger(self):
-       login = self.root.ids.login.text
-"""
-        
-
-
+    
 if __name__ == "__main__":
     MainApp().run()
